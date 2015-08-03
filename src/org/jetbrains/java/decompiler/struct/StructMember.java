7d5770f2462f354c4663f77818ae718062689d7b
cpw <cpw@weeksfamily.ca>
2015-08-02 20:56:16 -0400

    Merge any LVTT to an incoming LVT (if they're out of sequence), and merge LVTTs together if there's no LVT yet.

--- src/org/jetbrains/java/decompiler/struct/StructMember.java
+++ src/org/jetbrains/java/decompiler/struct/StructMember.java
@@ -62,6 +62,15 @@ public class StructMember {
           StructLocalVariableTableAttribute table = (StructLocalVariableTableAttribute)attributes.getWithKey(StructGeneralAttribute.ATTRIBUTE_LOCAL_VARIABLE_TABLE);
           table.addLocalVariableTable((StructLocalVariableTableAttribute)attribute);
         }
+        else if (StructGeneralAttribute.ATTRIBUTE_LOCAL_VARIABLE_TABLE.equals(name) && attributes.containsKey(StructGeneralAttribute.ATTRIBUTE_LOCAL_VARIABLE_TYPE_TABLE)) {
+            StructLocalVariableTableAttribute lvtt = (StructLocalVariableTableAttribute)attributes.getWithKey(StructGeneralAttribute.ATTRIBUTE_LOCAL_VARIABLE_TYPE_TABLE);
+            ((StructLocalVariableTableAttribute)attribute).addLocalVariableTable(lvtt);
+            attributes.addWithKey(attribute, attribute.getName());
+        }
+        else if (StructGeneralAttribute.ATTRIBUTE_LOCAL_VARIABLE_TYPE_TABLE.equals(name) && attributes.containsKey(StructGeneralAttribute.ATTRIBUTE_LOCAL_VARIABLE_TYPE_TABLE)) {
+            StructLocalVariableTableAttribute table = (StructLocalVariableTableAttribute)attributes.getWithKey(StructGeneralAttribute.ATTRIBUTE_LOCAL_VARIABLE_TYPE_TABLE);
+            table.addLocalVariableTable((StructLocalVariableTableAttribute)attribute);
+        }
         else {
           attributes.addWithKey(attribute, attribute.getName());
         }
